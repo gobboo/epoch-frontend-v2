1a4d9b17bcabcd581c7eff5b0ce3a5ef8b05d8de
Gobbo <yutubemoby@gmail.com>
2022-10-23 19:28:55 +0100

    fix: :bug: socket uses env

--- plugins/socket.client.ts
+++ plugins/socket.client.ts
@@ -2,7 +2,7 @@ import io from 'socket.io-client'
 import Client from '~~/composables/socket.client'
 
 export default defineNuxtPlugin(() => {
-    const socket: Client = new Client('http://localhost:3000');
+    const socket: Client = new Client(useRuntimeConfig().public.BASE_URL);
 
     return {
         provide: {
